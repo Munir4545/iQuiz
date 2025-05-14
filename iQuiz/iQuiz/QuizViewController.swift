a5494a653472dd5bf49d8954557a4da859241bb6
munir45 <memamy3@uw.edu>
2025-05-13 19:24:45 -0700

    part 2

---
+++ iQuiz/iQuiz/QuizViewController.swift
@@ -0,0 +1,148 @@
+//
+//  QuizViewController.swift
+//  iQuiz
+//
+//  Created by Munir Emam on 5/13/25.
+//
+
+import UIKit
+
+class QuizViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
+    
+    @IBOutlet weak var questionLabel: UILabel!
+    
+    @IBOutlet weak var answerLabel: UILabel!
+    
+    @IBOutlet weak var finalLabel: UILabel!
+    
+    @IBOutlet weak var answersTable: UITableView!
+    
+    @IBOutlet weak var questionStackView: UIStackView!
+    
+    @IBOutlet weak var answerStackView: UIStackView!
+    
+    @IBOutlet weak var finalStackView: UIStackView!
+    
+    var quiz: [[String: Any]] = []
+    
+    var question = [String: Any]()
+    
+    var answers: [String] = []
+    
+    var selectedAnswers: Int?
+    
+    var questionIndex: Int = 0
+    
+    var score: Int = 0
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        self.questionIndex = 0
+        self.score = 0
+        
+        self.question = quiz[questionIndex]
+        
+        if let text = question["question"] as? String {
+            questionLabel.text = text
+        }
+        
+        if let options = question["answers"] as? [String] {
+            answers = options
+        }
+        answersTable.delegate = self
+        answersTable.dataSource = self
+        answersTable.allowsMultipleSelection = false
+    }
+    
+    
+    @IBAction func submitQuestion(_ sender: Any) {
+        if selectedAnswers == nil {
+            let alert = UIAlertController(title: "answer", message: "you need to select an answer", preferredStyle: .alert)
+            alert.addAction(UIAlertAction(title: "Ok", style: .default))
+            present(alert, animated: true)
+        } else {
+            questionStackView.isHidden = true
+            answerStackView.isHidden = false
+            
+            if let answer = question["answer"] as? Int{
+                if answer == selectedAnswers {
+                    answerLabel.text = "Yay you got it correct. The answer is \(answers[answer])."
+                    answerLabel.textColor = UIColor.systemGreen
+                    score += 1
+                } else {
+                    answerLabel.text = "You got the question wrong. The answer is \(answers[answer])."
+                    answerLabel.textColor = UIColor.systemRed
+                }
+            }
+        }
+    }
+    
+    @IBAction func goNext(_ sender: Any) {
+        // go to next question and if not go to final screen
+        questionIndex += 1
+        
+        if questionIndex < quiz.count {
+            self.question = quiz[questionIndex]
+            if let text = question["question"] as? String {
+                questionLabel.text = text
+            }
+            
+            if let options = question["answers"] as? [String] {
+                answers = options
+            }
+            selectedAnswers = nil
+            answersTable.reloadData()
+            questionStackView.isHidden = false
+            answerStackView.isHidden = true
+        } else {
+            questionStackView.isHidden = true
+            answerStackView.isHidden = true
+            finalStackView.isHidden = false
+            
+            questionLabel.isHidden = true
+            
+            let totalScore = Double(score) / Double(quiz.count)
+            
+            if totalScore == 1.0 {
+                finalLabel.text = "Perfect! "
+            } else if totalScore > 0.75 {
+                finalLabel.text = "Almost there. "
+            } else if totalScore > 0.50 {
+                finalLabel.text = "Need more practice. "
+            } else {
+                finalLabel.text = "Terrible performance. "
+            }
+            
+            finalLabel.text! += "You got a score of \(score) / \(quiz.count)"
+
+        }
+    }
+    
+    @IBAction func finalGoNext(_ sender: Any) {
+        self.dismiss(animated: true, completion: nil)
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return answers.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "AnswerCell", for: indexPath)
+        cell.textLabel?.text = answers[indexPath.row]
+        
+        if indexPath.row == selectedAnswers {
+            cell.accessoryType = .checkmark
+        } else {
+            cell.accessoryType = .none
+        }
+        
+        return cell
+    }
+    
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        selectedAnswers = indexPath.row
+        tableView.reloadData()
+    }
+    
+}
--- iQuiz/iQuiz/ViewController.swift
+++ iQuiz/iQuiz/ViewController.swift
@@ -33,6 +33,24 @@ class ViewController: UIViewController, UITableViewDelegate {
         ["Marvel", "questions covering Marvel superheroes", "marvel.png"]
     ])
     
+    let questionData: [String: [[String: Any]]] = [
+        "Mathematics": [
+            ["question": "What is 5 x 5?", "answers": ["20", "25", "15", "95"], "answer": 1],
+            ["question": "What is 12 + 8?", "answers": ["18", "22", "20", "24"], "answer": 2],
+            ["question": "What is 100 / 10?", "answers": ["1", "10", "100", "0"], "answer": 1]
+        ],
+        "Science": [
+            ["question": "What is the chemical symbol for gold?", "answers": ["Ag", "Au", "He", "Go"], "answer": 1],
+            ["question": "What is H2O commonly known as?", "answers": ["Salt", "Air", "Water", "Sugar"], "answer": 2],
+            ["question": "Which planet is known as the Red Planet?", "answers": ["Earth", "Mars", "Jupiter", "Venus"], "answer": 1]
+        ],
+        "Marvel": [
+            ["question": "What is Spiderman's real name?", "answers": ["Tony Stark", "Bruce Banner", "Peter Parker", "Steve Rogers"], "answer": 2],
+            ["question": "What is Captain America's shield made of?", "answers": ["Adamantium", "Vibranium", "Promethium", "Steel"], "answer": 1],
+            ["question": "Who is the God of Thunder?", "answers": ["Loki", "Odin", "Heimdall", "Thor"], "answer": 3]
+        ]
+    ]
+    
     class iQuizTableDataModel : NSObject, UITableViewDataSource {
         
         let data: [[String]]
@@ -54,6 +72,8 @@ class ViewController: UIViewController, UITableViewDelegate {
         }
         
         
+        
+        
     }
     
     override func viewDidLoad() {
@@ -62,7 +82,25 @@ class ViewController: UIViewController, UITableViewDelegate {
         quizTable.dataSource = quizTableData
         quizTable.delegate = self
     }
-
-
+    
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        let quizName = quizTableData.data[indexPath.row][0]
+        performSegue(withIdentifier: "quizSegue", sender: quizName)
+    }
+    
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        if segue.identifier == "quizSegue" {
+            if let destinationVC = segue.destination as? QuizViewController {
+                if sender is String {
+                    segue.destination.modalPresentationStyle = .fullScreen
+                    
+                    if let questionsCategory = questionData[sender as! String] {
+                        destinationVC.quiz = questionsCategory
+                    }
+                }
+            }
+        }
+    }
+        
 }
 
